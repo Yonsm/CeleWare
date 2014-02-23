d6c1a54ebea9cf2664060eca58ac0fe6b7a4764f
Yonsm <Yonsm@msn.com>
2014-02-24 00:20:11 +0800

    HttPeek

--- HttPeek/Classes/AppStorePeek.mm
+++ HttPeek/Classes/AppStorePeek.mm
@@ -2,69 +2,7 @@
 
 #import "HookMain.h"
 
-FUNPTR(void, MSHookFunction, void *symbol, void *replace, void **result) = NULL;
-FUNPTR(void, MSHookMessageEx, Class _class, SEL sel, IMP imp, IMP *result) = NULL;
-FUNPTR(void *, SubstrateMemoryCreate, void *allocator, void *process, void *data, size_t size) = NULL;
-FUNPTR(void, SubstrateMemoryRelease, void * memory) = NULL;
-
-
-//
-#import <vector>
-#import <algorithm>
-
-//
-void LogRequest(NSURLRequest *request, void *returnAddress)
-{
-	static int s_index = 0;
-	static NSString *_logDir = nil;
-	static std::vector<NSURLRequest *> _requests;
-
-	if (_logDir == nil)
-	{
-		_logDir = [[NSString alloc] initWithFormat:@"/tmp/%@.req", NSProcessInfo.processInfo.processName];
-		[[NSFileManager defaultManager] createDirectoryAtPath:_logDir withIntermediateDirectories:YES attributes:nil error:nil];
-	}
-	
-	if ([request respondsToSelector:@selector(HTTPMethod)])
-	{
-		if (std::find(_requests.begin(), _requests.end(), request) == _requests.end())
-		{
-			_requests.push_back(request);
-			if (_requests.size() > 1024)
-			{
-				_requests.erase(_requests.begin(), _requests.begin() + 512);
-			}
-			
-			Dl_info info = {0};
-			dladdr(returnAddress, &info);
-			
-			NSString *str = [NSString stringWithFormat:@"FROM %s(%p)-%s(%p=>%#08lx)\n<%@>\n%@: %@\n%@\n\n", info.dli_fname, info.dli_fbase, info.dli_sname, info.dli_saddr, (long)info.dli_saddr-(long)info.dli_fbase-0x1000, [NSThread callStackSymbols], request.HTTPMethod, request.URL.absoluteString, request.allHTTPHeaderFields ? request.allHTTPHeaderFields : @""];
-			NSLog(@"HTTPEEK REQUEST: %@", str);
-			
-			NSString *file = [NSString stringWithFormat:@"%@/%d=%@.txt", _logDir, s_index++, NSUtil::UrlPath([request.URL.host stringByAppendingString:request.URL.path])];
-			if (request.HTTPBody.length && request.HTTPBody.length < 10240)
-			{
-				NSString *str2 = [[NSString alloc] initWithData:request.HTTPBody encoding:NSUTF8StringEncoding];
-				if (str2)
-				{
-					[[str stringByAppendingString:str2] writeToFile:file atomically:NO encoding:NSUTF8StringEncoding error:nil];
-					[str2 release];
-					return;
-				}
-			}
-			
-			[str writeToFile:file atomically:NO encoding:NSUTF8StringEncoding error:nil];
-			[request.HTTPBody writeToFile:[file stringByAppendingString:@".dat"] atomically:NO];
-		}
-	}
-}
-
-//
-void WebViewPeekInit(NSString *processName);
-void ConnectionPeekInit(NSString *processName);
-void ReadStreamPeekInit(NSString *processName);
-void ApplicationPeekInit(NSString *processName);
-
+#if 0
 //
 MSGHOOK(void, SUScriptInterface_gotoStoreURL_ofType_withAuthentication_forceAuthentication, id a3, id a4, char a5, char a6)
 {
@@ -128,6 +66,8 @@ MSGHOOK(void, SUClientApplicationController__handleAccountURL, id a3)
 	return _SUClientApplicationController__handleAccountURL(self, sel, a3);
 } ENDHOOK
 
+#endif
+
 //
 MSGHOOK(void, SUAccountViewController__mescalDidOpenWithSession_error, id a3, id a4)
 {
@@ -143,7 +83,7 @@ MSGHOOK(id, SUAccountViewController_init)
 	
 	NSString *str = [NSString stringWithFormat:@"FROM %s(%p)-%s(%p=>%#08lx)\n<%@>\n\n", info.dli_fname, info.dli_fbase, info.dli_sname, info.dli_saddr, (long)info.dli_saddr-(long)info.dli_fbase-0x1000, [NSThread callStackSymbols]];
 	NSLog(@"HTTPEEK SUAccountViewController_init: %@", str);
-
+	
 	NSLog(@"HTTPEEK:>>>SUAccountViewController_init");
 	return _SUAccountViewController_init(self, sel);
 } ENDHOOK
@@ -163,39 +103,27 @@ MSGHOOK(id, SUAccountViewController_initWithExternalAccountURL, id a3)
 
 
 //
-extern "C" void AppInit()
+void AppStorePeekInit(NSString *processName)
 {
-	@autoreleasepool
-	{
-		NSString *processName = NSProcessInfo.processInfo.processName;
-		_PTRFUN(/Library/Frameworks/CydiaSubstrate.framework/CydiaSubstrate, MSHookFunction);
-		_PTRFUN(/Library/Frameworks/CydiaSubstrate.framework/CydiaSubstrate, MSHookMessageEx);
-		
-		if (![processName isEqualToString:@"AppStore"] && ![processName isEqualToString:@"SUTest"]) return;
+	if (![processName isEqualToString:@"AppStore"] && ![processName isEqualToString:@"SUTest"]) return;
 	
-		NSLog(@"HTTPEEK new process %@ MSHookFunction=%p, MSHookMessageEx=%p", processName, _MSHookFunction, _MSHookMessageEx);
-				
-		//WebViewPeekInit(processName);
-		//ConnectionPeekInit(processName);
-		//ReadStreamPeekInit(processName);
-		//ApplicationPeekInit(processName);
-				
-		_HOOKMSG(SUAccountViewController_init, SUAccountViewController, init);
-//		_HOOKMSG(SUAccountViewController_initWithExternalAccountURL, SUAccountViewController, initWithExternalAccountURL:);
-//		_HOOKMSG(SUScriptInterface_gotoStoreURL_ofType_withAuthentication_forceAuthentication, SUScriptInterface, gotoStoreURL:ofType:withAuthentication:forceAuthentication:);
-//		
-//		_HOOKMSG(SUStorePageViewController__performActionForProtocolButton, SUStorePageViewController, _performActionForProtocolButton:);
-//		_HOOKMSG(SUStructuredPageViewController__gotoURLForItem_withURLIndex, SUStructuredPageViewController, _gotoURLForItem:withURLIndex:);
-//		_HOOKMSG(SUTermsAndConditionsView_alertView_didDismissWithButtonIndex, SUTermsAndConditionsView, alertView:didDismissWithButtonIndex:);
-//		_HOOKMSG(SUClient__newAccountViewControllerForButtonAction, SUClient, _newAccountViewControllerForButtonAction:);
-//		
-//		_HOOKMSG(SUScriptAccountPageViewController_newNativeViewController, SUScriptAccountPageViewController, newNativeViewController);
-//		_HOOKMSG(SUStoreController__handleAccountURL, SUStoreController, _handleAccountURL:);
-//		_HOOKMSG(SUClientController__newAccountViewControllerForButtonAction, SUClientController, _newAccountViewControllerForButtonAction:);
-//		_HOOKMSG(SUClientApplicationController__handleAccountURL, SUClientApplicationController, _handleAccountURL:);
-		
-		_HOOKMSG(SUAccountViewController__mescalDidOpenWithSession_error, SUAccountViewController, _mescalDidOpenWithSession:error:);
-		
-		return;
-	}
+	_HOOKMSG(SUAccountViewController_init, SUAccountViewController, init);
+	_HOOKMSG(SUAccountViewController_initWithExternalAccountURL, SUAccountViewController, initWithExternalAccountURL:);
+	_HOOKMSG(SUAccountViewController__mescalDidOpenWithSession_error, SUAccountViewController, _mescalDidOpenWithSession:error:);
+	
+#if 0
+	_HOOKMSG(SUScriptInterface_gotoStoreURL_ofType_withAuthentication_forceAuthentication, SUScriptInterface, gotoStoreURL:ofType:withAuthentication:forceAuthentication:);
+	
+	_HOOKMSG(SUStorePageViewController__performActionForProtocolButton, SUStorePageViewController, _performActionForProtocolButton:);
+	_HOOKMSG(SUStructuredPageViewController__gotoURLForItem_withURLIndex, SUStructuredPageViewController, _gotoURLForItem:withURLIndex:);
+	_HOOKMSG(SUTermsAndConditionsView_alertView_didDismissWithButtonIndex, SUTermsAndConditionsView, alertView:didDismissWithButtonIndex:);
+	_HOOKMSG(SUClient__newAccountViewControllerForButtonAction, SUClient, _newAccountViewControllerForButtonAction:);
+	
+	_HOOKMSG(SUScriptAccountPageViewController_newNativeViewController, SUScriptAccountPageViewController, newNativeViewController);
+	_HOOKMSG(SUStoreController__handleAccountURL, SUStoreController, _handleAccountURL:);
+	_HOOKMSG(SUClientController__newAccountViewControllerForButtonAction, SUClientController, _newAccountViewControllerForButtonAction:);
+	_HOOKMSG(SUClientApplicationController__handleAccountURL, SUClientApplicationController, _handleAccountURL:);
+#endif
+	
+	return;
 }
