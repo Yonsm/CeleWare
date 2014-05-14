116dd81529ccb14780fac94ab3e17b56939478f8
Yonsm <Yonsm@msn.com>
2014-05-14 14:57:01 +0800

    remove
    remove

--- BlueMate/Classes/Controller/Base/BaseController.h
+++ BlueMate/Classes/Controller/Base/BaseController.h
@@ -1,14 +1,6 @@
 
 #define kScrollViewBottomPad 30
 
-#import "DataLoader.h"
-
-//
-@protocol WizardControllerDelegate
-- (id)init;
-- (id)initWithParam:(id)param;	// 请注意：param 参数为 nil 时将调用 init 而不是 initWithParam
-@end
-
 //
 @interface BaseController : UIViewController
 {
