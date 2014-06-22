e2ee8cada2630ec3a0eb7976a86f78ae0f0ec49b
Yonsm <Yonsm@msn.com>
2014-06-22 22:40:03 +0800

    purify

--- BabyNes/Utilities/FoldPane/FacingView.h
+++
@@ -1,9 +0,0 @@
-
-#import "ShadowView.h"
-
-
-
-@interface FacingView : UIView
-@property (strong, nonatomic) ShadowView *shadowView;
-- (id)initWithFrame:(CGRect)frame foldDirection:(FoldDirection)foldDirection;
-@end
